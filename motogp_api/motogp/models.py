0b1e35e3f140029aa3568f0c936b444fa3a7c5fe
Agustina Guitler <agus.guitler@gmail.com>
2023-02-25 20:47:56 -0300

    Models, views and templates modified

--- motogp_api/motogp/models.py
+++ motogp_api/motogp/models.py
@@ -1,18 +1,20 @@
 from django.db import models
 
 class Team(models.Model):
+    id = models.AutoField(primary_key=True)
     team = models.CharField(max_length=30)
     riders = models.CharField(max_length=50, blank=True, null=True)
     bike = models.CharField(max_length=30)
     constructor_titles = models.IntegerField()
     sponsor = models.CharField(max_length=100)
-    history = models.TextField(max_length=300)
+    history = models.TextField()
     old_riders = models.TextField(max_length=500)
 
     def __str__(self):
-        return self.team, self.riders, self.bike, self.constructor_titles, self.sponsor, self.history, self.old_riders
+        return self.team
 
 class Rider(models.Model):
+    id = models.AutoField(primary_key=True)
     rider = models.CharField(max_length=50, blank=True, null=True)
     age = models.IntegerField()
     motogp_titles = models.IntegerField()
@@ -26,4 +28,4 @@ class Rider(models.Model):
     about_rider = models.TextField(max_length=500)
 
     def __str__(self):
-        return self.rider, self.age, self.motogp_titles, self.moto2_titles, self.moto3_titles, self.moto3_debut, self.moto2_debut, self.motogp_debut, self.team, self.old_teams, self.about_rider
+        return self.rider
